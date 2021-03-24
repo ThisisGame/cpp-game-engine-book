18b5f9ba9a968e22f718b84c4f04e5e75a79c529
captain <790621656@qq.com>
2021-03-25 01:07:26 +0800

    draw cube

--- samples/draw_cube/untitled/main.cpp
+++ samples/draw_cube/untitled/main.cpp
@@ -2,160 +2,16 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-#include "linmath.h"
+#include <glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtx/transform2.hpp>
+#include<glm/gtx/euler_angles.hpp>
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#pragma region 顶点坐标 顶点颜色
-
-struct Vector3
-{
-    float x,y,z;
-};
-
-struct Color
-{
-    float r, g, b;
-};
-
-static const Vector3 Positions[36] =
-{
-        //前
-        { -1.0f, -1.0f,1.0f},//左下
-        { 1.0f,  -1.0f,1.0f},//右下
-        { 1.0f,  1.0f,1.0f},//右上
-        {  1.0f, 1.0f,1.0f},//右上
-        { -1.0f, -1.0f,1.0f},//左上
-        { -1.0f, 1.0f,1.0f},//左下
-
-        //后
-        { -1.0f, -1.0f,-1.0f},//左下
-        {  1.0f, -1.0f,-1.0f},//右下
-        {   1.0f,  1.0f,-1.0f},//右上
-        {   1.0f,  1.0f,-1.0f},//右上
-        { -1.0f, -1.0f,-1.0f},//左上
-        { -1.0f,1.0f,-1.0f},//左下
-
-        //左
-        { -1.0f, -1.0f,-1.0f},//左下
-        {  -1.0f, -1.0f,1.0f},//右下
-        {   -1.0f,  1.0f,1.0f},//右上
-
-        {   -1.0f,  1.0f,1.0f},//右上
-        { -1.0f, 1.0f,-1.0f},//左上
-        { -1.0f, -1.0f,-1.0f},//左下
-
-        //右
-        { 1.0f, -1.0f,-1.0f},//左下
-        { 1.0f, -1.0f,1.0f},//右下
-        { 1.0f, 1.0f,1.0f},//右上
-
-        { 1.0f, 1.0f,1.0f},//右上
-        { 1.0f, 1.0f,-1.0f},//左上
-        { 1.0f, -1.0f,-1.0f},//左下
-
-        //上
-        { -1.0f, 1.0f,1.0f},//左下
-        { 1.0f,  1.0f,1.0f},//右下
-        { 1.0f,  1.0f,-1.0f},//右上
-
-        { 1.0f,  1.0f,-1.0f},//右上
-        { -1.0f, 1.0f,-1.0f},//左下
-        { -1.0f, 1.0f,1.0f},//左下
-
-        //下
-        { -1.0f, -1.0f,1.0f},//左下
-        { 1.0f,  -1.0f,1.0f},//右下
-        { 1.0f,  -1.0f,-1.0f},//右上
-
-        { 1.0f,  -1.0f,-1.0f},//右上
-        { -1.0f, -1.0f,-1.0f},//左下
-        { -1.0f, -1.0f,1.0f},//左下
-};
-
-static const Color Colors[36] =
-{
-        //前
-        { 1.f, 0.f, 0.f },//左下
-        { 1.f, 0.f, 0.f },//右下
-        { 1.f, 0.f, 0.f },//右上
-
-        { 1.f, 0.f, 0.f },//右上
-        { 1.f, 0.f, 0.f },//左上
-        { 1.f, 0.f, 0.f },//左下
-
-        //后
-        { 0.f, 1.f, 0.f },//左下
-        { 0.f, 1.f, 0.f },//右下
-        { 0.f, 1.f, 0.f },//右上
-
-        { 0.f, 1.f, 0.f },//右上
-        { 0.f, 1.f, 0.f },//左上
-        { 0.f, 1.f, 0.f },//左下
-
-        //左
-        { 0.f, 0.f, 1.f },//左下
-        { 0.f, 0.f, 1.f },//右下
-        { 0.f, 0.f, 1.f },//右上
-
-        { 0.f, 0.f, 1.f },//右上
-        { 0.f, 0.f, 1.f },//左上
-        { 0.f, 0.f, 1.f},//左下
-
-        //右
-        { 1.f, 1.f, 0.f },//左下
-        { 1.f, 1.f, 0.f },//右下
-        { 1.f, 1.f, 0.f },//右上
-
-        { 1.f, 1.f, 0.f },//右上
-        { 1.f, 1.f, 0.f },//左上
-        { 1.f, 1.f, 0.f },//左下
-
-        //上
-        { 0.f, 1.f, 1.f },//左下
-        { 0.f, 1.f, 1.f },//右下
-        { 0.f, 1.f, 1.f },//右上
-
-        { 0.f, 1.f, 1.f },//右上
-        { 0.f, 1.f, 1.f },//左上
-        { 0.f, 1.f, 1.f },//左下
-
-        //下
-        { 1.f, 0.f, 1.f },//左下
-        { 1.f, 0.f, 1.f },//右下
-        { 1.f, 0.f, 1.f },//右上
-
-        { 1.f, 0.f, 1.f },//右上
-        { 1.f, 0.f, 1.f },//左上
-        { 1.f, 0.f, 1.f},//左下
-};
-
-#pragma  endregion 顶点坐标 顶点颜色
-
-#pragma region 顶点Shader和片段Shader
-
-static const char* vertex_shader_text =
-        "#version 110\n"
-        "uniform mat4 MVP;\n"
-        "attribute vec3 vCol;\n"
-        "attribute vec3 vPos;\n"
-        "varying vec3 color;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = MVP * vec4(vPos, 1.0);\n"
-        "    color = vCol;\n"
-        "}\n";
-
-static const char* fragment_shader_text =
-        "#version 110\n"
-        "varying vec3 color;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_FragColor = vec4(color, 1.0);\n"
-        "}\n";
-
-#pragma endregion
+#include "VertexData.h"
+#include "ShaderSource.h"
 
 static void error_callback(int error, const char* description)
 {
@@ -228,12 +84,11 @@ int main(void)
     glEnableVertexAttribArray(vpos_location);
     glEnableVertexAttribArray(vcol_location);
 
-
     while (!glfwWindowShouldClose(window))
     {
         float ratio;
         int width, height;
-        mat4x4 m,v, p, mvp;
+        glm::mat4 model,view, projection, mvp;
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
@@ -241,34 +96,31 @@ int main(void)
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());//立方体绕Z轴旋转，导致只有朝向摄像机的一面被看到。
-        mat4x4_rotate_Y(m, m, (float) glfwGetTime());
-        mat4x4_rotate_X(m, m, (float) glfwGetTime());
+        glm::mat4 trans = glm::translate(glm::vec3(0,0,0)); //不移动顶点坐标;
+        static float rotate_eulerAngle=0.f;
+        rotate_eulerAngle+=1;
+        glm::mat4 rotation = glm::eulerAngleYXZ(glm::radians(rotate_eulerAngle), glm::radians(rotate_eulerAngle), glm::radians(rotate_eulerAngle)); //使用欧拉角旋转;
+        glm::mat4 scale = glm::scale(glm::vec3(2.0f, 2.0f, 2.0f)); //缩放;
+        model = trans*scale*rotation;
 
-        vec3 eye={0,0,10};
-        vec3 center={0,0,0};
-        vec3 up={0,1,0};
-        mat4x4_look_at(v,eye,center,up);
+        view = glm::lookAt(glm::vec3(0, 0, 10), glm::vec3(0, 0,0), glm::vec3(0, 1, 0));
 
-        mat4x4_ortho(p, -3, 3, -2.f, 2.f, -10.f, 10.f);
+        projection=glm::perspective(glm::radians(60.f),ratio,1.f,1000.f);
 
-        mat4x4_mul(mvp, p, m);
-//        mat4x4_mul(mvp, mvp, v);
-//        mat4x4_mul(mvp, mvp, p);
+        mvp=projection*view*model;
 
         //指定GPU程序(就是指定顶点着色器、片段着色器)
         glUseProgram(program);
             glEnable(GL_DEPTH_TEST);
             //上传顶点坐标数据
-            glVertexAttribPointer(vpos_location, 3, GL_FLOAT, false, sizeof(Vector3), Positions);
+            glVertexAttribPointer(vpos_location, 3, GL_FLOAT, false, sizeof(glm::vec3), Positions);
             //上传顶点颜色数据
-            glVertexAttribPointer(vcol_location, 3, GL_FLOAT, false, sizeof(Color), Colors);
+            glVertexAttribPointer(vcol_location, 3, GL_FLOAT, false, sizeof(glm::vec4), Colors);
             //上传mvp矩阵
-            glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+            glUniformMatrix4fv(mvp_location, 1, GL_FALSE, &mvp[0][0]);
 
             //void glDrawArrays(GLenum mode,GLint first,GLsizei count);
-            glDrawArrays(GL_TRIANGLES, 0, 6*6);//表示从第0个顶点开始画，总共画6个面，每个面6个顶点。
+            glDrawArrays(GL_TRIANGLES, (GLint)0, (GLsizei)6*6);//表示从第0个顶点开始画，总共画6个面，每个面6个顶点。
 
         glUseProgram(-1);
 
