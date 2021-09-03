56bcb5a8d244b0f8c9b06e5a6031be6c04454533
ThisisGame <980097191@qq.com>
2021-09-03 21:08:38 +0800

    integate lua

---
+++ samples/template/depends/LuaBridge/Array.h
@@ -0,0 +1,55 @@
+// https://github.com/vinniefalco/LuaBridge
+//
+// Copyright 2020, Dmitry Tarakanov
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <LuaBridge/detail/Stack.h>
+
+#include <array>
+
+namespace luabridge {
+
+template<class T, std::size_t s>
+struct Stack<std::array<T, s>>
+{
+    static void push(lua_State* L, std::array<T, s> const& array)
+    {
+        lua_createtable(L, static_cast<int>(s), 0);
+        for (std::size_t i = 0; i < s; ++i)
+        {
+            lua_pushinteger(L, static_cast<lua_Integer>(i + 1));
+            Stack<T>::push(L, array[i]);
+            lua_settable(L, -3);
+        }
+    }
+
+    static std::array<T, s> get(lua_State* L, int index)
+    {
+        if (!lua_istable(L, index))
+        {
+            luaL_error(L, "#%d argments must be table", index);
+            throw std::runtime_error("Array get () must receive a table");
+        }
+        if (index != s)
+        {
+            luaL_error(L, "array size should be %d ", s);
+        }
+
+        std::array<T, s> array;
+
+        int const absindex = lua_absindex(L, index);
+        lua_pushnil(L);
+        int arr_index = 0;
+        while (lua_next(L, absindex) != 0)
+        {
+            array[arr_index] = Stack<T>::get(L, -1);
+            lua_pop(L, 1);
+            arr_index++;
+        }
+        return array;
+    }
+};
+
+} // namespace luabridge
---
+++ samples/template/depends/LuaBridge/detail/Config.h
@@ -0,0 +1,7 @@
+// https://github.com/vinniefalco/LuaBridge
+// Copyright 2020, Dmitry Tarakanov
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Empty file
