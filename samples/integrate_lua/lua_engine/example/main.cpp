69e44e731f7d8e63df97636194b7daf56edea5c7
captain <790621656@qq.com>
2021-09-05 13:25:48 +0800

    lua engine

---
+++ samples/integrate_lua/lua_engine/example/main.cpp
@@ -0,0 +1,16 @@
+#define GLFW_INCLUDE_NONE
+
+#include "component/game_object.h"
+#include "utils/application.h"
+
+int main(void){
+    Application::set_title("[loadbank] press s play event,press 1 2 3 set param");
+    Application::set_data_path("../data/");//设置资源目录
+    Application::Init();//初始化引擎
+
+    GameObject* go=new GameObject("LoginSceneGo");
+    go->AddComponent("Transform");
+    go->AddComponent("LoginScene");
+
+    Application::Run();//开始引擎主循环
+}
