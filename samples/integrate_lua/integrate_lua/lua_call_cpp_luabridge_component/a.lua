20b7bf58df38b0e396054a1a0cb1af8037a61077
ThisisGame <980097191@qq.com>
2021-09-14 13:19:58 +0800

    lua component

--- samples/integrate_lua/integrate_lua/lua_call_cpp_luabridge_component/a.lua
+++ samples/integrate_lua/integrate_lua/lua_call_cpp_luabridge_component/a.lua
@@ -10,11 +10,34 @@ function LoginScene:Update()
 end
 
 function main()
-    go = GameObject()
-    go:AddComponent(Animator)
-    --go:AddComponent(Camera)
-    -- print(go)
+    local go = GameObject()
+    print("go:" .. tostring(go))
 
-    local loginScene=go:AddComponent(LoginScene)
-    print(loginScene)
+    --local animator=Animator()
+    --print("animator:" .. tostring(animator))
+    --
+    --animator:Awake()
+    --animator:set_game_object(go)
+    --print("animator:game_object():" .. tostring(animator:game_object()))
+
+    print("---------------------------")
+
+    animator=go:AddComponent("Animator")
+    print("animator:" .. tostring(animator))
+    animator:Awake()
+
+    local game_object=animator:game_object()
+    print("animator:game_object():" .. tostring(game_object))
+
+    print("---------------------------")
+
+    local camera=game_object:AddComponent("Camera")
+    print("camera:" .. tostring(camera))
+    camera:Awake()
+
+    game_object=camera:game_object()
+    print("camera:game_object():" .. tostring(game_object))
+
+    --local loginScene=go:AddComponent(LoginScene)
+    --print(loginScene)
 end
--- samples/integrate_lua/integrate_lua/lua_call_cpp_luabridge_component/main.cpp
+++ samples/integrate_lua/integrate_lua/lua_call_cpp_luabridge_component/main.cpp
@@ -19,7 +19,7 @@ lua_State* lua_state;
 class GameObject;
 class Component {
 public:
-    Component(){};
+    Component():game_object_(nullptr){};
     virtual ~Component(){};
 
     GameObject* game_object(){return game_object_;}
@@ -41,11 +41,7 @@ public:
     void Update() override{
         std::cout<<"Camera Update"<<std::endl;
     }
-
-public:
-    static std::string cpp_type_name;
 };
-std::string Camera::cpp_type_name="Camera";
 
 class Animator:public Component
 {
@@ -57,8 +53,6 @@ public:
     void Update() override{
         std::cout<<"Animator Update"<<std::endl;
     }
-public:
-    std::string cpp_type_name="Animator";
 };
 
 class GameObject{
@@ -70,37 +64,27 @@ public:
     ~GameObject() {
     }
 
-    template<typename T>
-    T* AddComponent(){
-        std::cout<<"GameObject AddComponent:"<<typeid(T).name()<<std::endl;
-        T* t=new T();
-        Component* component=dynamic_cast<Component*>(t);
-        component->set_game_object(this);
-        if(component_type_instance_map_.find(typeid(T).hash_code())==component_type_instance_map_.end()){
-            std::vector<Component*> component_vec;
-            component_vec.push_back(component);
-            component_type_instance_map_[typeid(T).hash_code()]=component_vec;
-        }else{
-            component_type_instance_map_[typeid(T).hash_code()].push_back(component);
-        }
-        component->Awake();
-        return t;
+    Component* AddComponent(std::string component_type_name){
+        std::cout<<"GameObject AddComponent:"<<component_type_name<<std::endl;
+        LuaRef lua_ref=AddComponentFromLua(component_type_name);
+        Component* component=lua_ref.cast<Component*>();
+        return component;
     }
 
-    template<typename T>
-    std::vector<Component*>& GetComponents() {
-        return component_type_instance_map_[typeid(T).hash_code()];
+
+    std::vector<Component*>& GetComponents(std::string component_type_name) {
+        return component_type_instance_map_[component_type_name];
     }
 
-    template<typename T>
-    T* GetComponent() {
-        if(component_type_instance_map_.find(typeid(T).hash_code())==component_type_instance_map_.end()){
+
+    Component* GetComponent(std::string component_type_name) {
+        if(component_type_instance_map_.find(component_type_name)==component_type_instance_map_.end()){
             return nullptr;
         }
-        if(component_type_instance_map_[typeid(T).hash_code()].size()==0){
+        if(component_type_instance_map_[component_type_name].size()==0){
             return nullptr;
         }
-        return dynamic_cast<T*>(component_type_instance_map_[typeid(T).hash_code()][0]);
+        return dynamic_cast<Component*>(component_type_instance_map_[component_type_name][0]);
     }
 
     void ForeachComponent(std::function<void(Component *)> func) {
@@ -120,34 +104,40 @@ public:
         }
     }
 public:
-    luabridge::LuaRef AddComponent(std::string component_type_name) {
+    luabridge::LuaRef AddComponentFromLua(std::string component_type_name) {
         luabridge::LuaRef component_type=luabridge::getGlobal(lua_state,component_type_name.c_str());
         auto new_table=component_type();//luabridge对c++的class注册为table，并实现了__call，所以可以直接带括号。
 
         if(new_table.isInstance<Component>()){
             Component* component=new_table.cast<Component*>();
             component->set_game_object(this);
-            if(cpp_component_type_instance_map_.find(component_type_name)==cpp_component_type_instance_map_.end()){
+            if(component_type_instance_map_.find(component_type_name)==component_type_instance_map_.end()){
                 std::vector<Component*> component_vec;
                 component_vec.push_back(component);
-                cpp_component_type_instance_map_[component_type_name]=component_vec;
+                component_type_instance_map_[component_type_name]=component_vec;
             }else{
-                cpp_component_type_instance_map_[component_type_name].push_back(component);
+                component_type_instance_map_[component_type_name].push_back(component);
             }
             component->Awake();
         }else{
-            new_table["Awake"]();
             new_table["set_game_object"](this);
+            if(lua_component_type_instance_map_.find(component_type_name)==lua_component_type_instance_map_.end()){
+                std::vector<luabridge::LuaRef> component_vec;
+                component_vec.push_back(new_table);
+                lua_component_type_instance_map_[component_type_name]=component_vec;
+            }else{
+                lua_component_type_instance_map_[component_type_name].push_back(new_table);
+            }
+            new_table["Awake"]();
         }
-
 
         return new_table;
     }
 
 private:
-    std::unordered_map<size_t,std::vector<Component*>> component_type_instance_map_;
 
-    std::unordered_map<std::string,std::vector<Component*>> cpp_component_type_instance_map_;
+    std::unordered_map<std::string,std::vector<Component*>> component_type_instance_map_;
+
     std::unordered_map<std::string,std::vector<luabridge::LuaRef>> lua_component_type_instance_map_;
 
     static std::list<GameObject*> game_object_list_;//存储所有的GameObject。
@@ -158,7 +148,7 @@ std::list<GameObject*> GameObject::game_object_list_;
 
 int main(int argc, char * argv[])
 {
-    GameObject* go=new GameObject();
+//    GameObject* go=new GameObject();
 //    go->AddComponent<Animator>();
 //    go->AddComponent<Camera>();
 
@@ -167,7 +157,7 @@ int main(int argc, char * argv[])
 //    std::cout<<typeid(GameObject).hash_code()<<std::endl;
 //    std::cout<<typeid(go).raw_name()<<std::endl;
 
-    GameObject* go1=new GameObject();
+//    GameObject* go1=new GameObject();
 //    std::cout<<typeid(go1).raw_name()<<std::endl;
 //    std::cout<<typeid(go).name()<<std::endl;// P10GameObject
 //    std::cout<<typeid(Animator).name()<<std::endl;// 8Animator
@@ -180,21 +170,23 @@ int main(int argc, char * argv[])
     luabridge::getGlobalNamespace(lua_state)
             .beginClass<Component>("Component")
             .addConstructor<void (*) ()>()
+            .addFunction("Awake",&Component::Awake)
+            .addFunction("Update",&Component::Update)
+            .addFunction("game_object",&Component::game_object)
+            .addFunction("set_game_object",&Component::set_game_object)
             .endClass();
     luabridge::getGlobalNamespace(lua_state)
             .beginClass<GameObject>("GameObject")
             .addConstructor<void (*) ()>()
-            .addFunction("AddComponent", (luabridge::LuaRef (GameObject::*)(luabridge::LuaRef))&GameObject::AddComponent)
+            .addFunction("AddComponent", (luabridge::LuaRef (GameObject::*)(std::string))&GameObject::AddComponent)
             .endClass();
     luabridge::getGlobalNamespace(lua_state)
             .deriveClass<Animator,Component>("Animator")
             .addConstructor<void (*) ()>()
-            .addData("cpp_type_name",&Animator::cpp_type_name)
             .endClass();
     luabridge::getGlobalNamespace(lua_state)
             .deriveClass<Camera,Component>("Camera")
             .addConstructor<void (*) ()>()
-            .addStaticData("cpp_type_name",&Camera::cpp_type_name)
             .endClass();
 
     luaL_dofile(lua_state, "../a.lua");
