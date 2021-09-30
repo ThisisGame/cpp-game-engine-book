521b9a75af010aa41f87ee2c7d3ee0529186c5f0
ThisisGame <980097191@qq.com>
2021-09-30 18:34:53 +0800

    draw font with freetype

---
+++ samples/draw_font/draw_ttf_font_freetype/source/utils/application.h
@@ -0,0 +1,38 @@
+//
+// Created by captainchen on 2021/5/14.
+//
+
+#ifndef UNTITLED_APPLICATION_H
+#define UNTITLED_APPLICATION_H
+
+#include <string>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+class Application {
+public:
+    static const std::string& data_path(){return data_path_;}
+    static void set_data_path(std::string data_path){data_path_=data_path;}
+
+    /// 初始化OpenGL
+    static void Init();
+
+    static void Run();
+
+    static void UpdateScreenSize();
+
+    /// 每一帧内逻辑代码。
+    static void Update();
+
+    /// 逻辑代码执行后，应用到渲染。
+    static void Render();
+
+private:
+    static std::string data_path_;//资源目录
+
+    static GLFWwindow* glfw_window_;
+
+};
+
+
+#endif //UNTITLED_APPLICATION_H
