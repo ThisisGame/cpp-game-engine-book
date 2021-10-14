3744c58056af44adca9af827f7b9cc3cb469e578
captain <790621656@qq.com>
2021-10-15 02:02:09 +0800

    UIMask

---
+++ samples/gui/ui_mask/source/component/component.cpp
@@ -0,0 +1,29 @@
+//
+// Created by captain on 2021/6/9.
+//
+
+#include "component.h"
+
+Component::Component() {
+
+}
+
+Component::~Component() {
+
+}
+
+void Component::Awake() {
+
+}
+
+void Component::Update() {
+
+}
+
+void Component::OnPreRender() {
+
+}
+
+void Component::OnPostRender() {
+
+}
