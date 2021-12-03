ee2348b7da60705cad67173ef4c39b282e59b884
ThisisGame <980097191@qq.com>
2021-12-03 20:43:15 +0800

    骨骼蒙皮动画

--- samples/integrate_lua/integrate_lua/lua_call_cpp_sol2/a.lua
+++ samples/integrate_lua/integrate_lua/lua_call_cpp_sol2/a.lua
@@ -4,6 +4,3 @@ print(player)
 print("player.hp_:" .. player.hp_)
 print("player.AddHp(4):" .. player:AddHp(4))
 print("player.hp_:" .. player.hp_)
-
-local data={1.0,2.0,3.0}
-player:CreateMesh(data,3)
--- samples/integrate_lua/integrate_lua/lua_call_cpp_sol2/main.cpp
+++ samples/integrate_lua/integrate_lua/lua_call_cpp_sol2/main.cpp
@@ -13,10 +13,6 @@ public:
     }
 
     int hp_=0;
-
-    void CreateMesh(float data[],int size){
-        float x=data[0];
-    }
 };
 
 int main(int argc, char * argv[])
@@ -28,15 +24,8 @@ int main(int argc, char * argv[])
     //注册成员函数和变量到lua
     usertype_player["AddHp"]=&Player::AddHp;
     usertype_player["hp_"]=&Player::hp_;
-    usertype_player["CreateMesh"]=&Player::CreateMesh;
 
-
-
-    auto result= sol_state.script_file("../a.lua");
-    if(result.valid()==false){
-        sol::error err = result;
-        std::cerr<<"\n---- LOAD LUA ERROR ----\n"<<err.what()<<std::endl;
-    }
+    sol_state.script_file("../a.lua");
 
     return 0;
 }
