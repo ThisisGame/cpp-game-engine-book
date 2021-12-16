1c4e4f22ef8a92b6e58961b9c5202cf080b74700
captain <790621656@qq.com>
2021-12-17 02:11:41 +0800

    导出骨骼蒙皮权重

--- files/skinned_mesh_renderer/export_skin.py
+++ files/skinned_mesh_renderer/export_skin.py
@@ -1,87 +1,38 @@
 # 导出骨骼权重，需要在Blender合并Mesh，然后选择Mesh，然后执行脚本。
 import bpy
-import bmesh
 import os
 import struct
-import json
-from mathutils import Matrix
-from math import radians
 
-# 引擎顶点结构(坐标、颜色、纹理坐标)
-class EngineVertex:
-    x=y=z=r=g=b=a=u=v=0
 
-    def __init__(self,x,y,z,r,g,b,a,u,v):
-        self.x=x
-        self.y=y
-        self.z=z
-        self.r=r
-        self.g=g
-        self.b=b
-        self.a=a
-        self.u=u
-        self.v=v
-    
-    def __eq__(self,other):
-        equal= self.x==other.x and self.y==other.y and self.z==other.z and self.r==other.r and \
-            self.g==other.g and self.b==other.b and self.a==other.a and self.u==other.u and self.v==other.v
-        return equal
+# 顶点关联骨骼及权重,每个顶点最多可以关联4个骨骼。
+class VertexRelateBoneInfo:
+    bone_index=[-1,-1,-1,-1] #骨骼索引，一般骨骼少于128个，用char就行。注意-1表示无骨骼。
+    bone_weight=[-1,-1,-1,-1]#骨骼权重，权重不会超过100，所以用char类型就可以。
 
-    def __str__(self) -> str:
-        return "x:{:.2f},y:{:.2f},z:{:.2f},r:{:.2f},g:{:.2f},b:{:.2f},a:{:.2f},u:{:.2f},v:{:.2f}".format(self.x,self.y,self.z,self.r,self.g,self.b,self.a,self.u,self.v)
+    def __init__(self):# 一定要在 __init__ 里面初始化一次，不然每次实例化返回的对象实例都是相同指针。
+        self.bone_index=[-1,-1,-1,-1]
+        self.bone_weight=[-1,-1,-1,-1]
 
-    def __repr__(self) -> str:#显示属性 用来打印。
+    def __str__(self):
+        return "bone_index:{},bone_weight:{}".format(self.bone_index,self.bone_weight)
+
+    def __repr__(self):
         return str(self)
-
+    
     def write(self,f):
-        f.write(struct.pack('f',self.x))
-        f.write(struct.pack('f',self.y))
-        f.write(struct.pack('f',self.z))
-        f.write(struct.pack('f',self.r))
-        f.write(struct.pack('f',self.g))
-        f.write(struct.pack('f',self.b))
-        f.write(struct.pack('f',self.a))
-        f.write(struct.pack('f',self.u))
-        f.write(struct.pack('f',self.v))
+        for i in range(4):# 写入4个骨骼索引
+            f.write(struct.pack('b',self.bone_index[i]))
+        for i in range(4):# 写入4个骨骼权重，注意权重是乘以100的。
+            if self.bone_weight[i]>=0:
+                f.write(struct.pack('b',int(self.bone_weight[i]*100)))
+            else:
+                f.write(struct.pack('b',-1))
 
-# 显示信息框
-def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
-    def draw(self, context):
-        self.layout.label(text=message)
-    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+print("-----------EXPORT WEIGHT---------")
 
 # 当前选中的Mesh
 object=bpy.context.object
 mesh = object.data
-uv_layer = mesh.uv_layers.active.data
-    
-print("---- COLLECT MESH -----")
-# 收集顶点
-mEngineVertexes=[]
-for v in mesh.vertices:
-    engine_vertex=EngineVertex(v.co.x,v.co.z,v.co.y,1,1,1,1,0,0)#构造引擎顶点，注意 y z调换
-    mEngineVertexes.append(engine_vertex)
-
-# 收集UV
-mEngineVertexIndexes=[]
-for poly in mesh.polygons:#遍历多边形
-    # print("Polygon index: %d, length: %d" % (poly.index, poly.loop_total))#检查是三角形还是四边形
-    if poly.loop_total==4:
-        ShowMessageBox("Need Triangle","Polygon Error",  'ERROR')
-        break
-    # 遍历一个多边形的3个顶点
-    for loop_index in range(poly.loop_start, poly.loop_start + poly.loop_total):
-        vertex_index=mesh.loops[loop_index].vertex_index #顶点在Blender中的索引
-        mEngineVertexIndexes.append(vertex_index)#把index放入顶点索引表
-
-        v=mesh.vertices[vertex_index] #拿顶点索引获取顶点
-        uv=uv_layer[loop_index].uv #获取当前顶点的UV坐标
-        engine_vertex=mEngineVertexes[vertex_index]#补全顶点UV数据
-        engine_vertex.u=uv.x
-        engine_vertex.v=uv.y
-
-print(*mEngineVertexes,sep='\n')
-print(mEngineVertexIndexes)
 
 print("---------- COLLECT BONE ------------")
 # 收集骨骼名字 [bone_name , bone_name , ...]
@@ -94,55 +45,45 @@ print(boneNames)
 
 print("---------- COLLECT WEIGHT ------------")
 # 收集骨骼权重
-armature_obj = bpy.data.objects["Armature"]
-obj_verts = mesh.vertices#当前选中的物体的所有顶点
+armature_obj = bpy.data.objects["Armature"]#场景中的骨架对象。
 
-obj_group_names = [g.name for g in object.vertex_groups]# 获取当前Mesh 关联的所有的顶点组名称
-
+obj_group_names = [g.name for g in object.vertex_groups]# 获取当前Mesh 关联的所有的顶点组名称。
 print(obj_group_names)
 
-for bone in armature_obj.pose.bones:
-    print(bone.name)
-    if bone.name not in obj_group_names:
-        continue
+vertex_relate_bone_infos=[]#顶点关联骨骼信息(4个骨骼索引、权重)，长度为顶点数。
 
-    # 获取骨骼的顶点组序号
-    gidx = object.vertex_groups[bone.name].index
-    # 获取骨骼的顶点组关联的顶点
-    bone_verts = [v for v in obj_verts if gidx in [g.group for g in v.groups]]
-    # 遍历顶点
-    for v in bone_verts:
-        for g in v.groups:
-            if g.group == gidx: 
-                w = g.weight
-                print("Vertex {} has a weight of {:.2f} for bone {}".format(v.index,w,bone.name))
+for vertex in mesh.vertices:#遍历当前选中的物体的所有顶点。
+    # print("Vertex index: %d" % vertex.index)
+    vertex_relate_bone_info=VertexRelateBoneInfo()
+    # print(vertex_relate_bone_info)
 
+    for group_index in range(0, len(vertex.groups)):#遍历顶点关联的顶点组。
+        group = vertex.groups[group_index]
+        group_name = obj_group_names[group.group]#获取顶点组名字
+        # print("Vertex group index: %d, group name: %s, weight: %f" % (group_index, group_name, group.weight))
+        if group_name in boneNames:#判断顶点组名字是否在骨骼名字列表中。制作的时候，建模人员要严格按照骨骼名字来作为顶点组名字。
+            bone_index=boneNames.index(group_name)#获取骨骼索引。
+            vertex_relate_bone_info.bone_index[group_index]=bone_index
+            vertex_relate_bone_info.bone_weight[group_index]=group.weight
+        else:
+            print("Error: group name not in bone names")
+    # print(vertex_relate_bone_info)
+    vertex_relate_bone_infos.append(vertex_relate_bone_info)
 
+print(*vertex_relate_bone_infos,sep='\n')
 
-
-# if os.path.exists("export")==False:
-#     os.mkdir("export")
+blender_project_dir = os.path.dirname(bpy.data.filepath)
+if os.path.exists(blender_project_dir+"/export")==False:
+    os.mkdir(blender_project_dir+"/export")
     
-# dir_path="export/"+object.name_full
+with open(blender_project_dir+"/export/export.weight", 'wb') as f:
+    #写入文件头
+    f.write("weight".encode())
 
-# if os.path.exists(dir_path)==False:
-#     os.mkdir(dir_path)
+    #写入顶点关联骨骼信息(4个骨骼索引、权重)，长度为顶点数。
+    for vertex_relate_bone_info in vertex_relate_bone_infos:
+        vertex_relate_bone_info.write(f)
     
-# with open(dir_path+"/"+object.name_full+'.mesh', 'wb') as f:
-#     #写入文件头
-#     f.write("mesh".encode())
-#     f.write(struct.pack('H',len(kVertexRemoveDumplicateVector)))
-#     f.write(struct.pack('H',len(kVertexIndexVector)))
+    f.close()
     
-#     #写入顶点数据
-#     for engine_vertex in kVertexRemoveDumplicateVector:
-#         engine_vertex.write(f)
-
-#     #写入索引数据
-#     kVertexIndexVectorInverse=kVertexIndexVector[::-1]
-#     for engine_vertex_index in kVertexIndexVectorInverse:
-#         f.write(struct.pack('H',engine_vertex_index))
-    
-#     f.close()
-    
-# print("----SUCCESS-----------------------------------")
+print("----SUCCESS-----------------------------------")
