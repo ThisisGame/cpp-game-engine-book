393f97e3706a22517fe2ce48778be25de20a8b58
captain <790621656@qq.com>
2021-12-25 00:59:36 +0800

    导出T-Pose

--- samples/load_fbx/extra_skinned_mesh/example/main.cpp
+++ samples/load_fbx/extra_skinned_mesh/example/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <filesystem>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <glm/gtx/string_cast.hpp>
@@ -60,6 +61,113 @@ namespace Engine{
                 file.write(reinterpret_cast<char*>(index_), sizeof(unsigned short) * head_.vertex_index_num_);
                 file.close();
             }
+        }
+    };
+
+    class Animation{
+    public:
+        std::string name_;
+        unsigned short frame_count_;//帧数
+        unsigned short frame_per_second_;//帧率
+
+        // 存储骨骼名字 [bone_name , bone_name , ...]
+        std::vector<std::string> bone_name_vec_;
+
+        // 存储骨骼父子关系，数组下标表示第几个Bone [[1],[]]
+        std::vector<std::vector<int>> bone_children_vec_;
+
+        // 存储骨骼 T-POSE，数组下标表示第几个Bone ["Bone.Matrix","Bone.001.Matrix"]
+        std::vector<glm::mat4> bone_t_pose_vec_;
+
+        // 存储骨骼关键帧数据，外面数组下标表示第几帧，里面数组下表表示第几个Bone [["Bone.Matrix","Bone.001.Matrix"] , ["Bone.Matrix","Bone.001.Matrix"]]
+        std::vector<std::vector<glm::mat4>> frame_bones_matrix_vec_;
+    public:
+        Animation(){
+            frame_count_ = 0;
+            frame_per_second_ = 0;
+        }
+
+        /// 获取骨骼index
+        int get_bone_index(std::string bone_name){
+            for (int i = 0; i < bone_name_vec_.size(); ++i) {
+                if(bone_name_vec_[i]==bone_name){
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        // 写入文件
+        void Write(const char* filePath){
+            if (std::filesystem::exists(filePath)==false){
+                DEBUG_LOG_INFO("{} not exist,will create.",filePath);
+                std::filesystem::path path(filePath);
+                if(path.has_filename()==false){
+                    DEBUG_LOG_ERROR("{} is not correct file path.",filePath);
+                    assert(false);
+                }
+                auto dir_path = std::filesystem::path{filePath}.parent_path();
+                std::error_code error_cord;
+                if(std::filesystem::create_directories(dir_path,error_cord)==false){
+                    DEBUG_LOG_ERROR("{} not exist,create failed.",filePath);
+                    assert(false);
+                }
+            }
+
+            std::ofstream file(filePath, std::ios::binary);
+            if(file.is_open()==false){
+                DEBUG_LOG_ERROR("file open failed:{}",filePath);
+                assert(false);
+                return;
+            }
+            // 写入文件头 skeleton_anim
+            std::string type = "skeleton_anim";
+            file.write(type.c_str(), type.size());
+            // 写入名字长度
+            unsigned short name_len = name_.size();
+            file.write(reinterpret_cast<char*>(&name_len), sizeof(unsigned short));
+            // 写入名字
+            file.write(name_.c_str(), name_.size());
+            // 写入帧数
+            file.write(reinterpret_cast<char*>(&frame_count_), sizeof(frame_count_));
+            // 写入帧率
+            file.write(reinterpret_cast<char*>(&frame_per_second_), sizeof(frame_per_second_));
+            // 写入骨骼数量
+            unsigned short bone_num = bone_name_vec_.size();
+            file.write(reinterpret_cast<char*>(&bone_num), sizeof(unsigned short));
+            // 写入骨骼名字
+            for (int i = 0; i < bone_name_vec_.size(); ++i) {
+                // 写入长度
+                unsigned short bone_name_len = bone_name_vec_[i].size();
+                file.write(reinterpret_cast<char*>(&bone_name_len), sizeof(unsigned short));
+                // 写入名字
+                file.write(bone_name_vec_[i].c_str(), bone_name_vec_[i].size());
+            }
+            // 写入骨骼父子关系
+            for (int i = 0; i < bone_children_vec_.size(); ++i) {
+                unsigned short children_num = bone_children_vec_[i].size();
+                file.write(reinterpret_cast<char*>(&children_num), sizeof(unsigned short));
+                for (int j = 0; j < bone_children_vec_[i].size(); ++j) {
+                    unsigned short child_index = bone_children_vec_[i][j];
+                    file.write(reinterpret_cast<char*>(&child_index), sizeof(unsigned short));
+                }
+            }
+            // 写入骨骼 T-POSE
+            for (int i = 0; i < bone_t_pose_vec_.size(); ++i) {
+                glm::mat4 bone_t_pose = bone_t_pose_vec_[i];
+                file.write(reinterpret_cast<char*>(&bone_t_pose), sizeof(glm::mat4));
+            }
+            // 写入关键帧帧数
+            unsigned short frame_num = frame_bones_matrix_vec_.size();
+            file.write(reinterpret_cast<char*>(&frame_num), sizeof(unsigned short));
+            // 写入骨骼关键帧数据
+            for (int frame_index = 0; frame_index < frame_bones_matrix_vec_.size(); ++frame_index) {
+                for (int bone_index = 0; bone_index < frame_bones_matrix_vec_[frame_index].size(); ++bone_index) {
+                    glm::mat4 bone_matrix = frame_bones_matrix_vec_[frame_index][bone_index];
+                    file.write(reinterpret_cast<char*>(&bone_matrix), sizeof(glm::mat4));
+                }
+            }
+            file.close();
         }
     };
 }
@@ -232,9 +340,38 @@ void ExtraWeight(const aiScene *scene) {// 遍历Material
     DEBUG_LOG_INFO("extra weight success");
 }
 
+
+
 /// 导出骨骼动画
 /// @param scene 导入的场景
 void ExtraAnimation(const aiScene *scene){
+    Engine::Animation engine_animation;
+
+    // 收集Bone名字
+    for (int i = 0; i < scene->mNumMeshes; ++i) {
+        Engine::MeshFile mesh_file;
+        aiMesh *mesh = scene->mMeshes[i]; // 获取Mesh
+        // 判断是否有骨骼
+        if (mesh->HasBones() == false) {
+            DEBUG_LOG_ERROR("Mesh {} has no bones", mesh->mName.C_Str());
+            return;
+        }
+        // 遍历Bone
+        for (int bone_index = 0; bone_index < mesh->mNumBones; ++bone_index) {
+            aiBone* bone = mesh->mBones[bone_index];
+            // 获取Bone名字
+            engine_animation.bone_name_vec_.push_back(bone->mName.C_Str());
+            // 获取Bone T-Pose
+            aiMatrix4x4& bone_matrix = bone->mOffsetMatrix;
+            engine_animation.bone_t_pose_vec_.push_back(glm::mat4(
+                    bone_matrix.a1, bone_matrix.a2, bone_matrix.a3, bone_matrix.a4,
+                    bone_matrix.b1, bone_matrix.b2, bone_matrix.b3, bone_matrix.b4,
+                    bone_matrix.c1, bone_matrix.c2, bone_matrix.c3, bone_matrix.c4,
+                    bone_matrix.d1, bone_matrix.d2, bone_matrix.d3, bone_matrix.d4));
+        }
+    }
+
+
     // 遍历Animation,一个FBX里面可能有多个Animation。
     for (int i = 0; i < scene->mNumAnimations; ++i) {
         aiAnimation* animation = scene->mAnimations[i];
@@ -243,33 +380,58 @@ void ExtraAnimation(const aiScene *scene){
         // 输出 Animation name:Take 001 duration:200 ticksPerSecond:30 channels:43
         // duration 总帧数，ticksPerSecond 每秒帧数
         DEBUG_LOG_INFO(str,animation->mName.C_Str(),animation->mDuration,animation->mTicksPerSecond,animation->mNumChannels);
+        engine_animation.name_=animation->mName.C_Str();
+        engine_animation.frame_count_=animation->mDuration;
+        engine_animation.frame_per_second_=animation->mTicksPerSecond;
+        for (int j = 0; j < engine_animation.frame_count_; ++j) {
+            engine_animation.frame_bones_matrix_vec_.push_back(std::vector<glm::mat4>());
+        }
 
         // 遍历Channel，一个Channel指一个Node的动画数据，这个Node可能是Bone，也可能是Mesh，也可能是空的父节点。
         // 把FBX拖到Blender里面Hierarchy显示的节点都会有一个Channel对应。
         // 当然Camera、Light这些Scene相关的是没有的。
         // 注意每个Channel的关键帧数量不一致。
         for (int channel_index = 0; channel_index < animation->mNumChannels; ++channel_index) {
-            aiNodeAnim* one_bone_anim = animation->mChannels[channel_index];
+            aiNodeAnim* node_anim = animation->mChannels[channel_index];
 //            DEBUG_LOG_INFO("Bone {} has {} keyframes", one_bone_anim->mNodeName.C_Str() , one_bone_anim->mNumPositionKeys);
             // 遍历一个Bone的动画数据:位移。
-            for (int key_index = 0; key_index < one_bone_anim->mNumPositionKeys; ++key_index) {
-                aiVectorKey& key = one_bone_anim->mPositionKeys[key_index];
+            for (int key_index = 0; key_index < node_anim->mNumPositionKeys; ++key_index) {
+                aiVectorKey& key = node_anim->mPositionKeys[key_index];
 //                DEBUG_LOG_INFO("Position: Key {}: time {}  value {}",key_index ,key.mTime ,glm::to_string(glm::vec3(key.mValue.x,key.mValue.y,key.mValue.z)));
             }
             // 遍历一个Bone的动画数据:旋转。
-            for (int key_index = 0; key_index < one_bone_anim->mNumRotationKeys; ++key_index) {
-                aiQuatKey& key = one_bone_anim->mRotationKeys[key_index];
+            for (int key_index = 0; key_index < node_anim->mNumRotationKeys; ++key_index) {
+                aiQuatKey& key = node_anim->mRotationKeys[key_index];
 //                DEBUG_LOG_INFO("Rotation: Key {}: time {}  value ({})",key_index ,key.mTime ,glm::to_string(glm::vec4(key.mValue.w,key.mValue.x,key.mValue.y,key.mValue.z)));
             }
             // 遍历一个Bone的动画数据:缩放。
-            for (int key_index = 0; key_index < one_bone_anim->mNumScalingKeys; ++key_index) {
-                aiVectorKey& key = one_bone_anim->mScalingKeys[key_index];
+            for (int key_index = 0; key_index < node_anim->mNumScalingKeys; ++key_index) {
+                aiVectorKey& key = node_anim->mScalingKeys[key_index];
 //                DEBUG_LOG_INFO("Scaling: Key {}: time {}  value {}",key_index ,key.mTime ,glm::to_string(glm::vec3(key.mValue.x,key.mValue.y,key.mValue.z)));
             }
             // 写到这里会发现，Assimp里存储的骨骼动画数据，只存储了关键帧数据，并不像之前在Blender里做的，将每一帧的数据都获取到了。
             // 所以从Assimp中导出的数据，在引擎中读取后，需要自己做插帧计算出当前帧的数据。
             // 插帧算法有很多种，我只做最简单的就是线性插帧，还有贝塞尔曲线插帧方式等，后续再去了解。
         }
+
+
+
+        // 计算所有帧的骨骼动画数据
+        for (int frame_index = 0; frame_index < animation->mDuration; ++frame_index) {
+            // 先填测试数据
+            for (int channel_index = 0; channel_index < animation->mNumChannels; ++channel_index) {
+                aiNodeAnim *node_anim = animation->mChannels[channel_index];
+                auto bone_index=engine_animation.get_bone_index(node_anim->mNodeName.C_Str());
+                if(bone_index==-1){
+                    DEBUG_LOG_ERROR("node {} is not bone.",node_anim->mNodeName.C_Str());
+                    continue;
+                }
+
+                engine_animation.frame_bones_matrix_vec_[frame_index].push_back(glm::mat4(1.0f));
+            }
+        }
+
+        engine_animation.Write(fmt::format("../data/animation/assimp_extra_{}.skeleton_anim", engine_animation.name_).c_str());
     }
 
     DEBUG_LOG_INFO("extra animation success");
@@ -281,9 +443,7 @@ int main(void){
     //实例化Assimp Importer
     Assimp::Importer importer;
 
-    // And have it read the given file with some example postprocessing
-    // Usually - if speed is not the most important aspect for you - you'll
-    // probably to request more postprocessing than we do in this example.
+    // 解析模型文件
     const aiScene* scene = importer.ReadFile( "../data/model/assimp_extra.fbx",aiProcess_CalcTangentSpace|aiProcess_Triangulate|aiProcess_JoinIdenticalVertices|aiProcess_SortByPType);
 
     // 读取文件失败
