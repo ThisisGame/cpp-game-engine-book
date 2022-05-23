0acdd9289716468ddc2e1a40667f83a1c521db76
captainchen <captainchen@tencent.com>
2022-05-23 19:24:31 +0800

    get subclass component

--- samples/integrate_lua/integrate_lua/lua_call_cpp_sol2_component_lua_wrap/example/main.lua
+++ samples/integrate_lua/integrate_lua/lua_call_cpp_sol2_component_lua_wrap/example/main.lua
@@ -9,6 +9,7 @@ require("login_scene")
 game_object=nil
 
 function main()
+    print(is_sub_class_of(Animator,GameObject))
     game_object = GameObject.new()
     print("game_object:" .. tostring(game_object))
 
--- samples/integrate_lua/integrate_lua/lua_call_cpp_sol2_component_lua_wrap/source_lua/lua_extension.lua
+++ samples/integrate_lua/integrate_lua/lua_call_cpp_sol2_component_lua_wrap/source_lua/lua_extension.lua
@@ -21,57 +21,37 @@ function class(classname, super)
     local superType = type(super)
     local cls
 
-    if superType ~= "function" and superType ~= "table" then
+    if superType ~= "table" then
         superType = nil
         super = nil
     end
 
-    if superType == "function" or (super and super.__ctype == 1) then
-        -- inherited from native C++ Object
-        cls = {}
-
-        if superType == "table" then
-            -- copy fields from super
-            for k,v in pairs(super) do cls[k] = v end
-            cls.__create = super.__create
-            cls.super    = super
-        else
-            cls.__create = super
-        end
-
-        cls.ctor    = function() end
-        cls.__cname = classname
-        cls.__ctype = 1
-
-        function cls.new(...)
-            local instance = cls.__create(...)
-            -- copy fields from class to native object
-            for k,v in pairs(cls) do instance[k] = v end
-            instance.class = cls
-            instance:ctor(...)
-            return instance
-        end
-
+    -- inherited from Lua Object
+    if super then
+        cls = clone(super)
+        cls.super = super
     else
-        -- inherited from Lua Object
-        if super then
-            cls = clone(super)
-            cls.super = super
-        else
-            cls = {ctor = function() end}
-        end
+        cls = {ctor = function() end}
+    end
 
-        cls.__cname = classname
-        cls.__ctype = 2 -- lua
-        cls.__index = cls
+    cls.__cname = classname
+    cls.__index = cls
 
-        function cls.new(...)
-            local instance = setmetatable({}, cls)
-            instance.class = cls
-            instance:ctor(...)
-            return instance
-        end
+    function cls.new(...)
+        local instance = setmetatable({}, cls)
+        instance.class = cls
+        instance:ctor(...)
+        return instance
     end
 
     return cls
+end
+
+--- 判断继承关系
+function is_sub_class_of(sub,super)
+    if sub.__index.super==super then
+        return true
+    else
+        return is_sub_class_of(sub,sub.__index.super)
+    end
 end
