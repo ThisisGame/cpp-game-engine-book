98bf4e26cb1800f76035b3de5bf7fc17b74aa49a
captainchen <captainchen@tencent.com>
2022-06-09 18:00:31 +0800

    diffuse lighting

--- samples/classic_lighting/diffuse/source/lighting/light.cpp
+++ samples/classic_lighting/diffuse/source/lighting/light.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "light.h"
+#include <rttr/registration>
+
+using namespace rttr;
+RTTR_REGISTRATION//注册反射
+{
+    registration::class_<Light>("Light")
+            .constructor<>()(rttr::policy::ctor::as_raw_ptr);
+}
+
+Light::Light():Component() {}
+
+Light::~Light() {}
