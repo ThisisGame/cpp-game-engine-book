c7544f7674180b66176ec211de4dbf9a3ef9501e
captain <790621656@qq.com>
2022-06-13 00:54:54 +0800

    specular highlight

---
+++ samples/classic_lighting/specular_highlight/source/lighting/environment.cpp
@@ -0,0 +1,10 @@
+﻿//
+// Created by captainchen on 2022/6/7.
+//
+
+#include "environment.h"
+
+Environment::Environment():
+    ambient_color_(glm::vec3(1.0f, 1.0f, 1.0f)),ambient_color_intensity_(1.0f) {
+
+}
---
+++ samples/classic_lighting/specular_highlight/source/lighting/environment.h
@@ -0,0 +1,33 @@
+﻿//
+// Created by captainchen on 2022/6/7.
+//
+
+#ifndef INTEGRATE_PHYSX_ENVIRONMENT_H
+#define INTEGRATE_PHYSX_ENVIRONMENT_H
+
+#include <glm/glm.hpp>
+
+/// 环境配置
+class Environment {
+public:
+    Environment();
+
+    /// 设置环境颜色
+    void set_ambient_color(const glm::vec3 &ambient_color){ambient_color_= ambient_color;}
+
+    /// 获取环境颜色
+    const glm::vec3 &ambient_color() const{return ambient_color_;}
+
+    /// 设置环境强度
+    void set_ambient_color_intensity(float ambient_color_intensity){ambient_color_intensity_ = ambient_color_intensity;}
+
+    /// 获取环境强度
+    float ambient_color_intensity() const{return ambient_color_intensity_;}
+
+private:
+    glm::vec3 ambient_color_;// 环境颜色
+    float ambient_color_intensity_;// 环境颜色强度
+};
+
+
+#endif //INTEGRATE_PHYSX_ENVIRONMENT_H
