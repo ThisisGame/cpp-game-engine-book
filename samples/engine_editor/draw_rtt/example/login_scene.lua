f7b5577a7af0d05c587573c98c67099db431b56a
captain <790621656@qq.com>
2022-10-30 14:03:48 +0800

    DLSS

--- samples/engine_editor/draw_rtt/example/login_scene.lua
+++ samples/engine_editor/draw_rtt/example/login_scene.lua
@@ -130,7 +130,7 @@ function LoginScene:CreateMainCamera()
     self.camera_:SetPerspective(60, Screen:aspect_ratio(), 1, 1000)
     --设置RenderTexture
     self.render_texture_ = RenderTexture.new()
-    self.render_texture_:Init(960,640)
+    self.render_texture_:Init(240,160)
     self.camera_:set_target_render_texture(self.render_texture_)
 end
 
@@ -183,6 +183,17 @@ function LoginScene:CreateUI()
     -- 挂上 UIImage 组件，将RenderTexture绘制出来
     local ui_image_mod_bag=self.go_ui_image_rtt_:AddComponent(UIImage)
     ui_image_mod_bag:set_texture(self.render_texture_:color_texture_2d())
+
+    -- 创建一个UI，UI不缩放，使用原生分辨率渲染
+    self.go_ui_image_=GameObject.new("native draw image")
+    self.go_ui_image_:set_layer(2)
+    -- 设置尺寸
+    local rect_transform=self.go_ui_image_:AddComponent(RectTransform)
+    rect_transform:set_position(glm.vec3(0,-200,0))
+    rect_transform:set_rect(glm.vec2(128,128))
+    -- 挂上 UIImage 组件
+    local ui_image_btn=self.go_ui_image_:AddComponent(UIImage)
+    ui_image_btn:set_texture(Texture2D.LoadFromFile("images/btn_power.cpt"))
 end
 
 function LoginScene:Update()
