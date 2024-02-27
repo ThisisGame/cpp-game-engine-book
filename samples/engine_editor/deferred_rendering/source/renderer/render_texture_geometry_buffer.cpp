cb6396836b354f807b54f81bc0e1ed8662c8480e
captainchen <captainchen@tencent.com>
2024-02-27 20:41:56 +0800

    deferred rendering

---
+++ samples/engine_editor/deferred_rendering/source/renderer/render_texture_geometry_buffer.cpp
@@ -0,0 +1,38 @@
+﻿//
+// Created by captainchen on 2022/8/19.
+//
+
+#include "render_texture_geometry_buffer.h"
+#include "renderer/texture_2d.h"
+#include "render_device/gpu_resource_mapper.h"
+#include "render_device/render_task_producer.h"
+
+RenderTextureGeometryBuffer::RenderTextureGeometryBuffer(): RenderTexture(), frag_position_texture_2d_(nullptr), frag_normal_texture_2d_(nullptr), frag_color_texture_2d_(nullptr) {
+}
+
+RenderTextureGeometryBuffer::~RenderTextureGeometryBuffer() {
+    if(frame_buffer_object_handle_>0){
+        RenderTaskProducer::ProduceRenderTaskDeleteFBO(frame_buffer_object_handle_);
+    }
+    //删除Texture2D
+    if(frag_position_texture_2d_ != nullptr){
+        delete frag_position_texture_2d_;
+    }
+    if(frag_normal_texture_2d_ != nullptr){
+        delete frag_normal_texture_2d_;
+    }
+    if(frag_color_texture_2d_ != nullptr){
+        delete frag_color_texture_2d_;
+    }
+}
+
+void RenderTextureGeometryBuffer::Init(unsigned short width, unsigned short height) {
+    width_=width;
+    height_=height;
+    frag_position_texture_2d_=Texture2D::Create(width_, height_, GL_RGBA, GL_RGB, GL_FLOAT, nullptr, 0);
+    frag_normal_texture_2d_=Texture2D::Create(width_, height_, GL_RGBA, GL_RGB, GL_FLOAT, nullptr, 0);
+    frag_color_texture_2d_=Texture2D::Create(width_, height_, GL_RGBA, GL_RGB, GL_FLOAT, nullptr, 0);
+    //创建FBO任务
+    frame_buffer_object_handle_ = GPUResourceMapper::GenerateFBOHandle();
+    RenderTaskProducer::ProduceRenderTaskCreateGBuffer(frame_buffer_object_handle_, width_, height_, frag_position_texture_2d_->texture_handle(), frag_normal_texture_2d_->texture_handle(), frag_color_texture_2d_->texture_handle());
+}
---
+++ samples/engine_editor/deferred_rendering/source/utils/time.cpp
@@ -0,0 +1,33 @@
+﻿//
+// Created by captain on 2021/8/5.
+//
+
+#include "time.h"
+
+std::chrono::system_clock::time_point Time::startup_time_;
+float Time::delta_time_=0;
+float Time::last_frame_time_=0;
+float Time::fixed_update_time_=1.0/60;
+
+Time::Time() {
+}
+
+Time::~Time() {
+}
+
+void Time::Init() {
+    startup_time_= std::chrono::system_clock::now();
+}
+
+void Time::Update() {
+    if(last_frame_time_>0){
+        delta_time_=TimeSinceStartup()-last_frame_time_;
+    }
+    last_frame_time_=TimeSinceStartup();
+}
+
+float Time::TimeSinceStartup() {
+    std::chrono::time_point now = std::chrono::system_clock::now();
+    uint64_t ms=std::chrono::duration_cast<std::chrono::milliseconds>(now - startup_time_).count();
+    return ms/1000.0f;
+}
